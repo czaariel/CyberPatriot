dc4356e2657ce769e7cc5b2c4dbee74557403133
czaariel <58752683+czaariel@users.noreply.github.com>
2020-05-28 16:31:00 -0400

    Update mainwindowscript.ps1

--- Scripts/mainwindowscript.ps1
+++ Scripts/mainwindowscript.ps1
@@ -166,7 +166,16 @@ Set-Content config wuauserv start= auto
 	    Get-CimInstance -Query 'Select * from Win32_UserAccount where name LIKE "$User" -Property @{PasswordExpires=$True}
 	}
 	#Change Passwords
-	[array]$usersonthiscomp = (Read-Host "List all users on this computer (separated by only a comma. Ex: tom,bob,joe)").split(",") | %{$_.trim()}
+	$usersonthiscomp = (Read-Host "List all users on this computer (separated by comma)").split(",") | %{$_.trim()}
+
+	for ($i=0; $i -lt $usersonthiscomp.length; $i++) {
+	    #Clear-Host
+	    Write-Output $usersonthiscomp[$i] is a user
+	    $deletethisuser = Read-Host Delete $usersonthiscomp[$i]? y or n
+	    if ( $deletethisuser -eq "y") {
+		Write-Output $usersonthiscomp[$i] is being deleted
+	    }
+	}
 	
 # Password Policy
     #Set minimum password length to 8, max password age to 90 days, minimum age to 15 days, and how many passwords are kept to prevent reuse
