33434d072fc547e066fe530761541747686ebc9e
czaariel <58752683+czaariel@users.noreply.github.com>
2020-06-06 09:22:03 -0400

    Update uselesswindowscript.ps1

--- Scripts/uselesswindowscript.ps1
+++ Scripts/uselesswindowscript.ps1
@@ -1,49 +1,64 @@
+Write-Output "Hello"
 
 
-Param()
- 
-$prompt = "Enter the user's SAMAccountname"
-$Title = "Reset Password"
-$Default = $null
- 
-Add-Type -AssemblyName "microsoft.visualbasic" -ErrorAction Stop
-#use a VBScript style input box to prompt for the user name
-$username = [microsoft.visualbasic.interaction]::InputBox($Prompt,$Title,$Default)
- 
-if ($username) {
-    #prompt for the new password
-    $prompt = "Enter the user's new password"
-    $Plaintext =[microsoft.visualbasic.interaction]::InputBox($Prompt,$Title,$Default)
- 
-    #convert to secure string
-    $NewPassword = ConvertTo-SecureString -String $Plaintext -AsPlainText -Force
- 
-    #define a hash table of parameter values to splat to 
-    #Set-ADAccountPassword
-    $paramHash = @{
-    Identity = $Username
-    NewPassword = $NewPassword 
-    Reset = $True
-    Passthru = $True
-    ErrorAction = "Stop"
-    }
- 
-    Try {
-     $output = Set-ADAccountPassword @paramHash |
-     Set-ADUser -ChangePasswordAtLogon $True -PassThru |
-     Get-ADuser -Properties PasswordLastSet,PasswordExpired,WhenChanged | Out-String
- 
-     #display user in a message box
-     $message = $output
-     $button = "OKOnly"
-     $icon = "Information"
-     [microsoft.visualbasic.interaction]::Msgbox($message,"$button,$icon",$title) | Out-Null
-    }
-    Catch {
-        #display error in a message box
-        $message =  "Failed to reset password for $Username. $($_.Exception.Message)"
-        $button = "OKOnly"
-        $icon = "Exclamation"
-       [microsoft.visualbasic.interaction]::Msgbox($message,"$button,$icon",$title) | Out-Null
-    }
-} #if user specified
+function cont {
+    
+	$contyn = Read-Host "Continue? answer with y or n"
+	if ( $contyn -eq "n") {
+		Write-Output "Exitng"
+		exit
+	}
+	
+}
+
+function runAll {
+
+	updates
+	firewall
+	regconfig
+	userconfig
+	passwordpol
+	audits
+	removeprograms
+
+}
+
+
+function updates {
+
+	Write-Output "Configuring updates..."
+	Install-Module PSWindowsUpdate
+	Add-WUServiceManager -MicrosoftUpdate
+	Install-WindowsUpdate -MicrosoftUpdate -AcceptAll 
+	Write-Output "You can visit https://www.techrepublic.com/article/how-to-use-powershell-to-manage-microsoft-updates-on-windows/ if you want to learn more..."
+	cont
+    
+
+
+}
+
+function firewall {
+
+}
+
+function regconfig {
+
+}
+
+function userconfig {
+
+}
+
+function passwordpol {
+
+}
+
+function audits {
+
+}
+
+function removeprograms {
+
+}
+
+runAll
